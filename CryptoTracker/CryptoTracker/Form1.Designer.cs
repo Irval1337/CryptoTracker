0c562802668903d3295a8028cdc3993cd02e98c1
Irval <56792892+Irval1337@users.noreply.github.com>
2022-08-09 22:55:18 +0300

    Update Form1.Designer.cs

--- CryptoTracker/CryptoTracker/Form1.Designer.cs
+++ CryptoTracker/CryptoTracker/Form1.Designer.cs
@@ -317,6 +317,7 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(382, 342);
             this.listBox1.TabIndex = 16;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // checkBox2
             // 
